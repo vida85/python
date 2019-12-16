d8a5d54d20582d03a9da82f7017052c23f2a005e
vida85 <46607821+vida85@users.noreply.github.com>
2019-12-16 05:34:07 +0000

    Create morse_coder.py

---
+++ morse_coder.py
@@ -0,0 +1,13 @@
+ #! python3
+ # Morse Coder by Davi Silveira
+
+morse_code = {' ': ' | ', 'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..-.', 'g': '--.', 'h': '....', 'i': '..', 'j': '.---', 'k': '-.-', 'l': '.-..', 'm': '--', 'n': '-.', 'o': '---', 'p': '.--.', 'q': '--.-','r': '.-.', 's': '...', 't': '-','u': '..-','v': '...-','w': '.--','x': '-..-','y': '-.--','z': '--..'}
+coded = ''
+msg = input('Enter your message: ').lower()
+
+for char in msg:
+    for morse in morse_code.keys():
+        if char == morse:
+            coded += morse_code[morse] + ' '
+
+print(f'Orginal: {msg}\nCoded: {coded}')
