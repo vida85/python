d9051810cca0f08b8a65468d9e2a5678157926f9
vida85 <46607821+vida85@users.noreply.github.com>
2019-12-17 18:00:10 +0000

    Update morse_coder.py

--- morse_coder.py
+++ morse_coder.py
@@ -1,22 +1,19 @@
  #! python3
  # Morse Coder by Davi Silveira
 
-morse_code = {' ': ' | ', 'a': '.-', 'b': '-...',
-              'c': '-.-.', 'd': '-..', 'e': '.',
-              'f': '..-.', 'g': '--.', 'h': '....',
-              'i': '..', 'j': '.---', 'k': '-.-',
-              'l': '.-..', 'm': '--', 'n': '-.',
-              'o': '---', 'p': '.--.', 'q': '--.-',
-              'r': '.-.', 's': '...', 't': '-',
-              'u': '..-','v': '...-','w': '.--',
-              'x': '-..-','y': '-.--','z': '--..'
-              }
+morse_code = {' ': ' | ', 'a': '.-', 'b': '-...', 'c': '-.-.',
+              'd': '-..', 'e': '.', 'f': '..-.', 'g': '--.',
+              'h': '....', 'i': '..', 'j': '.---', 'k': '-.-',
+              'l': '.-..', 'm': '--', 'n': '-.', 'o': '---',
+              'p': '.--.', 'q': '--.-','r': '.-.', 's': '...',
+              't': '-','u': '..-','v': '...-','w': '.--',
+              'x': '-..-', 'y': '-.--','z': '--..'}
+
 coded = ''
 msg = input('Enter your message: ').lower()
 
 for char in msg:
-    for morse in morse_code.keys():
-        if char == morse:
-            coded += morse_code[morse] + ' '
+    if char in morse_code.keys():
+        coded += morse_code[char] + ' '
 
 print(f'Orginal: {msg}\nCoded: {coded}')
