fab1aa4c0ded4f3e3e29d1a3f72b49aecc154b73
vida85 <46607821+vida85@users.noreply.github.com>
2019-12-24 03:01:01 +0000

    Update Probably_Knot.py

--- Probably_Knot.py
+++ Probably_Knot.py
@@ -136,7 +136,7 @@ class Ui_MainWindow(object):
         wrd = self.wrd_line.text().lower()
         
         if txt != '':
-            API_KEY = 'a701e74e453ee6695e450310340401f5'
+            API_KEY = 'YOUR API KEY' <-- ## add your own API key here!!! ##
             URL = f'http://words.bighugelabs.com/api/2/{API_KEY}/{wrd}/json'
             
             # RELATIONSHIP_ABBR = {'syn':'Synonyms','ant':'Antonyms','rel':'Related terms',
